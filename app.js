71f214e5fe0a82a25dbc5e7c8ee72e58b8d03192
Chase <Gcrowder93@gmail.com>
2021-09-29 16:25:35 -0700

    all my changed because i forgot to commit while working

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-// import functions and grab DOM elements
+const emuImg = document.getElementById('emu');
+const emuAudio = document.getElementById('emu-sound');
+const emuspan = document.getElementById('emuspan');
+const sheepImg = document.getElementById('sheep');
+const sheepAudio = document.getElementById('sheep-sound');
+const sheepspan = document.getElementById('sheepspan');
+const whaleImg = document.getElementById('whale');
+const whaleAudio = document.getElementById('whale-sound');
+const whalespan = document.getElementById('whalespan');
 
-// initialize global state
+let emuclicks = 0;
+let sheepclicks = 0;
+let whaleclicks = 0;
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+emuImg.addEventListener('click', () => {
+    emuAudio.play();
+    emuclicks ++;
+
+    emuspan.textContent = 'Fingers you would have lost..' + emuclicks;
+  
+
+});
+
+sheepImg.addEventListener('click', () => {
+    sheepAudio.play();
+    sheepclicks ++;
+    sheepspan.textContent = 'Number of pets..' + sheepclicks;
+
+});
+
+whaleImg.addEventListener('click', () => {
+    whaleAudio.play();
+    whaleclicks ++;
+
+    whalespan.textContent = 'Number of whales youll never touch..' + whaleclicks;
+
+});
